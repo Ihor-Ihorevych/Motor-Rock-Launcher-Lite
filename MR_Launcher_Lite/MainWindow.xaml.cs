c9541eb499e49f3e6a8f35559e0f95574db5075c
Ihor-Ihorevych <77419426+Ihor-Ihorevych@users.noreply.github.com>
2022-01-12 15:36:41 +0200

    controls update
    You can set game folder from now, and so on, launcher will write it to config

--- MR_Launcher_Lite/MainWindow.xaml.cs
+++ MR_Launcher_Lite/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +14,98 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace MR_Launcher_Lite
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region Fields
+        private Models.Config _config;
+        private string _selectedModVersion;
+        #endregion
+        #region Properties
+        public Models.Config Config
+        {
+            get { return _config; }
+            set
+            {
+                _config = value;
+                OnPropertyChanged(nameof(Config));
+            }
+        }
+        public string SelectedModVersion
+        {
+            get => _selectedModVersion;
+            set
+            {
+                _selectedModVersion = value;
+                OnPropertyChanged(nameof(SelectedModVersion));
+            }
+        }
+        #endregion
+        #region Window init
         public MainWindow()
         {
             InitializeComponent();
+            Init();
         }
+        private async void Init()
+        {
+            // Setting context
+            DataContext = this;
+
+            // Setting fields
+            _config = new();
+            _selectedModVersion = string.Empty;
+
+            // Trying to load config
+            var isLoaded = await Config.Load();
+            if (!isLoaded)
+            {
+                MessageBox.Show("Error while reading config, overwriting");
+                Config.Save();
+            }
+        }
+        #endregion
+        #region Controls clicks
+        private void Locate_Game_Folder_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new()
+            {
+                Filter = "Motor Rock executable|*.exe",
+                Title = "Choose Motor Rock executable file"
+            };
+            if (fileDialog.ShowDialog() == true)
+            {
+                if(fileDialog.SafeFileName.ToLower() == "mr.exe")
+                {
+                    Config.GameLocation = fileDialog.FileName;
+                    Config.Save();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong file!");
+                }
+            }
+        }
+        private void Install_Selected_Mod_Click(object sender, EventArgs e)
+        {
+            if(SelectedModVersion == string.Empty)
+            {
+                MessageBox.Show("Choose mod version first");
+            }
+        }
+        #endregion
+        #region INotify
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
     }
 }
---
+++ MR_Launcher_Lite/Models/Config.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace MR_Launcher_Lite.Models
+{
+    public class Config : INotifyPropertyChanged
+    {
+        #region Fields
+        private string _gameLocation;
+        private ObservableCollection<string> _mods;
+        #endregion
+        #region Properties
+        public string GameLocation
+        {
+            get => _gameLocation;
+            set
+            {
+                _gameLocation = value;
+                OnPropertyChanged(nameof(GameLocation));
+            }
+        }
+        public ObservableCollection<string> Mods
+        {
+            get => _mods;
+            set
+            {
+                _mods = value;
+                OnPropertyChanged(nameof(Mods));
+            }
+        }
+        #endregion
+        #region Init
+        public Config()
+        {
+            _gameLocation = string.Empty;
+            _mods = new() { "a", "b", "c"};
+        }
+        #endregion
+        #region Methods
+        public async void Save()
+        {
+            var serializedObject = JsonSerializer.Serialize(this);
+            await File.WriteAllTextAsync("config.json", serializedObject);
+        }
+        public async Task<bool> Load()
+        {
+            try
+            {
+                string json = await File.ReadAllTextAsync("config.json");
+                var deserializedObject = JsonSerializer.Deserialize<Config>(json);
+                GameLocation = deserializedObject.GameLocation;
+                Mods = deserializedObject.Mods;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+        #region INotify
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+    }
+}
